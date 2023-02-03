4e837f4cc7ed731de72d089c305fe3b1a3685b59
Guilherme Silva Andrade <guisilvandrade@gmail.com>
2023-02-03 13:24:27 -0300

    First connection established with the server

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import PowerTriangle from './components/PowerTriangle';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PowerTriangle /> 
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Canivete Elétrico</h1>
+    </div>
+  )
+}
+
+export default Header;
